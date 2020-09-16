dd64735a2e9404e5dc882439c7cb1adae1fdbf5d
Giancarlo Pernudi Segura <giancarlopernudi@hotmail.com>
2020-09-16 15:30:18 -0600

    instagram section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,12 +3,17 @@ div
 	section.section
 		h1.title About Us
 		lorem(add="2p")
+	section.section
+		h1.title Instagram
 </template>
 
 <script>
 import LoremIpsum from 'vue-lorem-ipsum/src/LoremIpsum.vue'
 
 export default {
+	head: {
+		title: 'About Us'
+	},
 	components: {
 		lorem: LoremIpsum
 	}
